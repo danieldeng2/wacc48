9a94cf4a9a206e2eb8ec56271139ecc2f1a86a70
danieldeng2 <danieldeng223@gmail.com>
2021-01-25 23:27:02 +0000

    Add sample class and test for continuous integration

---
+++ src/main/kotlin/Calculator.kt
@@ -0,0 +1,12 @@
+import java.lang.IllegalArgumentException
+
+class Calculator {
+    fun parse(s: String): Int {
+        val (a, op, b) = s.split(" ")
+        return when (op) {
+            "*" -> a.toInt() * b.toInt()
+            "/" -> a.toInt() / b.toInt()
+            else -> throw IllegalArgumentException("invalid operator")
+        }
+    }
+}
