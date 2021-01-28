b31bf5a177b5fd75b43e7650315fbfed758a9306
Madi Baig <madibaig18@yahoo.com>
2021-01-28 17:52:33 +0000

    Implemented parser rules
    It works however test suite hasn't been done yet for the parser. A few changes were added to WACCLexer.g4.
    jek1918 & mb2319 & zd419

---
+++ src/test/kotlin/WACCLexerTest.kt
@@ -0,0 +1,28 @@
+import org.antlr.runtime.Token
+import org.junit.Test
+
+class WACCLexerTest {
+
+    //fun getTokensFromString(string: String): List<Token> {
+        // Requires to run ANTLR first to build java classes
+
+//        val byteStream = string.byteInputStream()
+//        val charStream = CharStreams.fromStream(byteStream)
+//        val lexer = WACCLexer(charStream)
+//        val tokenStream = CommonTokenStream(lexer)
+//        tokenStream.fill()
+//        return tokenStream.tokens
+
+    //}
+
+    @Test
+    fun exampleTest() {
+//        val tokens = getTokensFromString("1 + 2 + 3")
+
+//        assertEquals(WACCLexer.INT, tokens[0].type)
+//        assertEquals(WACCLexer.PLUS, tokens[1].type)
+        // ...
+    }
+
+    // tests a given string gives a correct list of tokens
+}
