b6ebfcc330558e01aab9d225cadf181a76be7de9
Jordan Kisa <jordan.kisa18@imperial.ac.uk>
2021-02-03 14:22:38 +0000

    Throws exception for syntax errors Extended BaseErrorListener to throw ParseCancellationException Outputs relevant message of offending symbol jek1918

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,33 @@
 import org.antlr.v4.runtime.*
 
 import antlr.*
+import org.antlr.v4.runtime.misc.ParseCancellationException
+
 
 fun main(args: Array<String>) {
     val input: CharStream = CharStreams.fromStream(System.`in`)
     val lexer = WACCLexer(input)
+    lexer.removeErrorListeners();
+    lexer.addErrorListener(ThrowingErrorListener());
     val tokens = CommonTokenStream(lexer)
     val parser = WACCParser(tokens)
+    parser.removeErrorListeners()
+    parser.addErrorListener(ThrowingErrorListener())
     val tree = parser.prog()
 
     println(tree.toStringTree(parser))
 }
+
+class ThrowingErrorListener() : BaseErrorListener() {
+    override fun syntaxError(
+        recognizer: Recognizer<*, *>?,
+        offendingSymbol: Any?,
+        line: Int,
+        charPositionInLine: Int,
+        msg: String?,
+        e: RecognitionException?
+    ) {
+        throw ParseCancellationException("line $line:$charPositionInLine $msg");
+    }
+}
+
