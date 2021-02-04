30ee59d6c41141044c842e1d17954065006ab223
hhv19 <hhv19@ic.ac.uk>
2021-02-04 16:42:59 +0000

    Use kotlin built-in max/min int value

--- src/main/kotlin/analyser/nodes/type/IntType.kt
+++ src/main/kotlin/analyser/nodes/type/IntType.kt
@@ -4,13 +4,11 @@ import analyser.SymbolTable
 import kotlin.math.pow
 
 object IntType : Type {
-    private const val minus2: Double = -2.0
-    private const val two: Double = 2.0
-    val min: Int = minus2.pow(31).toInt()
-    val max: Int = two.pow(31).toInt() - 1
 
-    override fun validate(st: SymbolTable) {
-    }
+    const val min = Int.MIN_VALUE
+    const val max = Int.MAX_VALUE
+
+    override fun validate(st: SymbolTable) {}
 
     override fun toString(): String {
         return "Int"
