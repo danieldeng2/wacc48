6a42baf83687dae2c499e49b59c617ba088d85c8
hhv19 <hhv19@ic.ac.uk>
2021-02-04 23:36:40 +0000

    FunctionCall searches for function name up the tree until root

--- src/main/kotlin/analyser/nodes/function/FuncCallNode.kt
+++ src/main/kotlin/analyser/nodes/function/FuncCallNode.kt
@@ -13,7 +13,7 @@ data class FuncCallNode(
     override var type: Type = VoidType
 
     override fun validate(st: SymbolTable) {
-        if (st.containsInCurrentScope(name))
+        if (!st.containsInAnyScope(name))
             throw SemanticsException("Cannot find function $name")
         val functionNode = st[name] as FuncNode
 
