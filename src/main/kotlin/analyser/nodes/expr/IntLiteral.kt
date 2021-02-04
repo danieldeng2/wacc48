8036ee36f7643ed325ccc6379af4bab289ee64bc
hhv19 <hhv19@ic.ac.uk>
2021-02-04 23:36:06 +0000

    Fix integer range to be inclusive of extreme values

--- src/main/kotlin/analyser/nodes/expr/IntLiteral.kt
+++ src/main/kotlin/analyser/nodes/expr/IntLiteral.kt
@@ -9,7 +9,7 @@ data class IntLiteral(val value: Int) : ExprNode {
     override var type: Type = IntType
 
     override fun validate(st: SymbolTable) {
-        if (value > IntType.max || value < IntType.min)
+        if (value >= IntType.max || value <= IntType.min)
             throw SemanticsException("IntLiteral $value is out of range")
     }
 }
