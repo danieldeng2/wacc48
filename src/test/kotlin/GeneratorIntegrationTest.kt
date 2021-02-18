3fa98b74f1e5f2ac037f586dc6e98338c159ae56
danieldeng2 <danieldeng223@gmail.com>
2021-02-18 19:40:19 +0000

    Refactor GeneratorIntegrationTest for ease adding new tests

--- src/test/kotlin/GeneratorIntegrationTest.kt
+++ src/test/kotlin/GeneratorIntegrationTest.kt
@@ -12,11 +12,49 @@ class GeneratorIntegrationTest {
     var totalTests = 0
     var passedTests = 0
 
-    private fun compilerPipeline(path: String, stdin: String): EmulatorResult {
+    @Test
+    fun basicProgramsShouldMatchReferenceOutput() {
+        checkAllMatches("valid/basic")
+    }
+
+    private fun checkAllMatches(label: String) {
+        val dir = File(object {}.javaClass.getResource(label).file)
+        dir.walk().forEach { f ->
+            if (f.isFile) {
+                totalTests++
+                println("${f.path}:")
+                val compilerResult = compilerPipeline(f.path)
+                val referenceResult = referencePipeline(f.path)
+
+                when {
+                    compilerResult.emulatorOut != referenceResult.emulatorOut ->
+                        println("Expected ${referenceResult.emulatorOut} but got ${compilerResult.emulatorOut}")
+                    compilerResult.emulatorExit != referenceResult.emulatorExit ->
+                        println("Expected exit code ${referenceResult.emulatorExit} but got ${compilerResult.emulatorExit}")
+                    else ->
+                        passedTests++
+                }
+            }
+        }
+        println("$passedTests/$totalTests tests passed for programs in $label")
+        assertEquals(totalTests, passedTests)
+    }
+
+    private fun compilerPipeline(path: String, stdin: String = ""): EmulatorResult {
         val input = CharStreams.fromFileName(path)
         val pNode = runAnalyser(input)
         val assembly = runGenerator(pNode)
 
+        return executeAssembly(assembly, stdin)
+    }
+
+    private fun referencePipeline(path: String, stdin: String = ""): EmulatorResult {
+        val assembly = RefCompiler(File(path)).run()
+
+        return executeAssembly(assembly, stdin)
+    }
+
+    private fun executeAssembly(assembly: List<String>, stdin: String): EmulatorResult {
         val assemFile = File("tmp.s")
         val writer = FileWriter(assemFile)
         assembly.forEach { writer.write(it + System.lineSeparator()) }
@@ -24,50 +62,6 @@ class GeneratorIntegrationTest {
 
         val res = RefEmulator(assemFile).execute(stdin)
         assemFile.delete()
-
         return res
-    }
-
-    private fun referencePipeline(path: String, stdin: String): EmulatorResult {
-        val assembly = RefCompiler(File(path)).run()
-
-        val assemFile = File("tmp_ref.s")
-        val writer = FileWriter(assemFile)
-        assembly.forEach { writer.write(it + System.lineSeparator()) }
-        writer.close()
-
-        val res = RefEmulator(assemFile).execute(stdin)
-        assemFile.delete()
-
-        return res
-    }
-
-
-    @Test
-    fun basicProgramsShouldMatchReferenceOutput() {
-        val dir = File(object {}.javaClass.getResource("valid/basic").file)
-
-        dir.walk().forEach { f ->
-            if (f.isFile) {
-                totalTests++
-                println("${f.path}:")
-                val compilerResult = compilerPipeline(f.path, "")
-                val referenceResult = referencePipeline(f.path, "")
-
-                when {
-                    compilerResult.emulatorOut != referenceResult.emulatorOut -> {
-                        println("Expected ${referenceResult.emulatorOut} but got ${compilerResult.emulatorOut}")
-                    }
-                    compilerResult.emulatorExit != referenceResult.emulatorExit -> {
-                        println("Expected exit code ${referenceResult.emulatorExit} but got ${compilerResult.emulatorExit}")
-                    }
-                    else -> {
-                        passedTests++
-                    }
-                }
-            }
-        }
-        println("$passedTests/$totalTests tests passed for basic programs")
-        assertEquals(totalTests, passedTests)
     }
 }
