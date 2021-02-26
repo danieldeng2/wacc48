033a598bd0dc9d2cd1042232ba0713df1d0ab722
hhv19 <hhv19@ic.ac.uk>
2021-02-26 18:16:05 +0000

    Implement codegen for free pairs

--- src/main/kotlin/analyser/nodes/statement/FreeNode.kt
+++ src/main/kotlin/analyser/nodes/statement/FreeNode.kt
@@ -4,6 +4,10 @@ import analyser.SymbolTable
 import analyser.nodes.expr.ExprNode
 import analyser.nodes.type.GenericPair
 import exceptions.SemanticsException
+import generator.instructions.Instruction
+import generator.instructions.branch.BLInstr
+import generator.translator.TranslatorContext
+import generator.translator.lib.FreePair
 import org.antlr.v4.runtime.ParserRuleContext
 
 data class FreeNode(
@@ -21,4 +25,12 @@ data class FreeNode(
         if (value.type !is GenericPair)
             throw SemanticsException("Cannot free ${value.type}", ctx)
     }
+
+    override fun translate(ctx: TranslatorContext) =
+        mutableListOf<Instruction>().apply {
+            ctx.addLibraryFunction(FreePair)
+
+            addAll(value.translate(ctx))
+            add(BLInstr(FreePair.label))
+        }
 }
