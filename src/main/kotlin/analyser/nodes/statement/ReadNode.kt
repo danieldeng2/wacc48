29877e1cb5b0d2978d2d94c0d908716180282a2f
Madi Baig <madibaig18@yahoo.com>
2021-02-26 20:13:24 +0000

    Implemented reading integers to pass all while tests.

--- src/main/kotlin/analyser/nodes/statement/ReadNode.kt
+++ src/main/kotlin/analyser/nodes/statement/ReadNode.kt
@@ -2,12 +2,23 @@ package analyser.nodes.statement
 
 import analyser.SymbolTable
 import analyser.nodes.assignment.LHSNode
-import analyser.nodes.type.CharType
-import analyser.nodes.type.IntType
-import analyser.nodes.type.StringType
-import analyser.nodes.type.Type
+import analyser.nodes.expr.IdentifierNode
+import analyser.nodes.type.*
 import exceptions.SemanticsException
+import generator.instructions.Instruction
+import generator.instructions.arithmetic.ADDInstr
+import generator.instructions.branch.BLInstr
+import generator.instructions.move.MOVInstr
+import generator.instructions.operands.NumOp
+import generator.instructions.operands.Register
+import generator.translator.TranslatorContext
+import generator.translator.lib.print.PrintBool
+import generator.translator.lib.print.PrintInt
+import generator.translator.lib.print.PrintLn
+import generator.translator.lib.print.PrintStr
+import generator.translator.lib.read.ReadInt
 import org.antlr.v4.runtime.ParserRuleContext
+import java.rmi.UnexpectedException
 
 data class ReadNode(
     private val value: LHSNode,
@@ -24,4 +35,26 @@ data class ReadNode(
         if (value.type !in expectedExprTypes)
             throw SemanticsException("Cannot read from type ${value.type}", ctx)
     }
+
+    override fun translate(ctx: TranslatorContext) =
+        mutableListOf<Instruction>().apply {
+            addAll(value.translate(ctx))
+
+            if (value is IdentifierNode) {
+                add(ADDInstr(Register.R0, Register.SP, NumOp(ctx.getOffsetOfLocalVar(value.name, st))))
+            } else {
+                TODO("Implement getting the memory address of ${value.type}")
+            }
+
+            when (value.type) {
+                IntType, CharType -> {
+                    val readFunc = when (value.type) {
+                        IntType -> ReadInt
+                        else -> TODO("Implement read for ${value.type}")
+                    }
+                    ctx.addLibraryFunction(readFunc)
+                    add(BLInstr(readFunc.label))
+                }
+            }
+        }
 }
