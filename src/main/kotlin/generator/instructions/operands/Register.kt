cf3a956de4eadb862141963dbdafed0584c90bc6
hhv19 <hhv19@ic.ac.uk>
2021-02-27 14:55:01 +0000

    Add new instructions for 'array' code generator

--- src/main/kotlin/generator/instructions/operands/Register.kt
+++ src/main/kotlin/generator/instructions/operands/Register.kt
@@ -6,7 +6,9 @@ enum class Register(val repr: String) : LoadableOp, WritableOp {
     SP("sp"),
     R0("r0"),
     R1("r1"),
-    R2("r2");
+    R2("r2"),
+    R3("r3"),
+    R4("r4");
 
     override fun toString() = repr
 }
