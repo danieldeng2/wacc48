6aa02a1cdba7c3b45fe664994f80d2e0456273cd
hhv19 <hhv19@ic.ac.uk>
2021-03-04 17:04:32 +0000

    Adds comments for the codegeneration visitor

--- src/main/kotlin/generator/translator/ArmConstants.kt
+++ src/main/kotlin/generator/translator/ArmConstants.kt
@@ -4,4 +4,6 @@ object ArmConstants {
     const val OPERAND2_MAX_VALUE = 1024
     const val NUM_BYTE_ADDRESS = 4
     const val NULL_ADDRESS = 0
+    const val TRUE_VALUE = 1
+    const val FALSE_VALUE = 0
 }
