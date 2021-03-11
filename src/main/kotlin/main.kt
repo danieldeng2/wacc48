481b4d9817ccdbb7ed7918b312eb384d4989102d
hhv19 <hhv19@ic.ac.uk>
2021-03-11 12:05:50 +0000

    Modify main to allow users to choose between x86 and ARM asm

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,21 +1,31 @@
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.path
 import org.antlr.v4.runtime.CharStream
 import org.antlr.v4.runtime.CharStreams
-import java.io.File
 import java.io.FileWriter
-import java.nio.file.Path
 
-fun main(args: Array<String>) {
-    if (args.isEmpty() || !File(args[0]).exists()) {
-        println("Usage: compile <WACC Source>")
-        return
+class ArgParse : CliktCommand() {
+    private val arm by option("-arm", help = "Generate code for ARM11").flag()
+    private val sourcePath by argument(help = "Path to WACC file").path(
+        mustExist = true,
+        canBeFile = true,
+        canBeDir = false
+    )
+
+    override fun run() {
+        echo("WACC Compiler - Group 48")
+
+        val input: CharStream = CharStreams.fromPath(sourcePath)
+        val pNode = runAnalyserCatchError(input)
+        val output = runGenerator(pNode, armAssembly = arm)
+        writeResult(sourcePath.fileName.toString(), output)
     }
-
-    val sourceFile = Path.of(args[0])
-    val input: CharStream = CharStreams.fromPath(sourceFile)
-    val pNode = runAnalyserCatchError(input)
-    val output = runGenerator(pNode, armAssembly = false)
-    writeResult(sourceFile.fileName.toString(), output)
 }
+
+fun main(args: Array<String>) = ArgParse().main(args)
 
 fun writeResult(inputName: String, output: List<String>) {
     val outName = inputName.replace(".wacc", ".s")
